f4bb4b1b620750d846738345835044e2f43a06a6
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-09-05 08:46:18 +0530

    added base appshell

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <section
+    class="
+      max-w-7xl
+      min-h-screen
+      mx-auto
+      py-16
+      px-4
+      sm:py-24 sm:px-6
+      lg:px-8
+      font-poppins
+    "
+  >
+    <div class="md:flex md:items-center md:justify-between">
+      <div class="flex-1 min-w-0">
+        <h2 class="text-2xl font-bold leading-7 sm:text-3xl sm:truncate">
+          Virtual cards
+        </h2>
+      </div>
+      <div class="mt-4 flex md:mt-0 md:ml-4">
+        <button
+          type="button"
+          class="
+            ml-3
+            inline-flex
+            items-center
+            px-4
+            py-2
+            border border-transparent
+            rounded-sm
+            text-sm
+            font-medium
+            bg-white
+            shadow-md
+            focus:outline-none
+            focus:ring-2
+            focus:ring-offset-2
+            focus:ring-offset-pink-500
+            focus:ring-pink-100
+          "
+        >
+          + Virtual card
+        </button>
+      </div>
+    </div>
+    
+  </section>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
